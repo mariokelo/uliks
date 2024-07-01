400f8828395b6acf24a46da7bd9f44ede255b008
visionmakers-jehone <152643837+visionmakers-jehone@users.noreply.github.com>
2024-07-01 12:58:31 +0200

    mesime12.js

--- script/mesime12.js
+++ script/mesime12.js
@@ -43,6 +43,11 @@ function mesime12() {
         console.error('An unknown error occurred.');
     }
   }
+  document.onkeydown = (e) => {
+  if (e.ctrlKey && e.key == 'u'){
+    e.preventDefault();
+  }
+}
 }
 
 const audio = new Audio('/audio/CFARE MESIMI DO TE DEGJOSH.mp3')
